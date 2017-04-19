b48604fc91208109c6144483a6a3777e026d5fa0
matt-clement <clement.matthewp@gmail.com>
2017-04-19 00:21:50 -0400

    Split RLE components into their own module

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,7 @@ use std::io;
 use std::io::Write;
 use std::io::stdout;
 
-#[derive(Debug,PartialEq)]
-enum RleComponent {
-    Run(usize, u8),
-    Literal(Vec<u8>)
-}
+mod run_length_encoding;
 
 fn main() {
     let mut input = String::new();
@@ -15,119 +11,8 @@ fn main() {
     stdout().flush().unwrap();
     io::stdin().read_line(&mut input).unwrap();
 
-    let compressed = rle(&input.trim());
+    let compressed = run_length_encoding::encode(&input.trim());
     println!("{:?}", compressed);
-    let decompressed = rld(compressed);
+    let decompressed = run_length_encoding::decode(compressed);
     println!("{:?}", decompressed);
-}
-
-fn rle(input: &str) -> Vec<RleComponent> {
-    let mut last : u8 = input.bytes().nth(0).unwrap();
-    let mut counter : usize = 0;
-    let mut encoded = vec!();
-
-    for bv in input.bytes() {
-        if bv == last {
-            counter += 1;
-        } else {
-            if counter == 1 {
-                // TODO: Combine adjacent literals.
-                encoded.push(RleComponent::Literal(vec![last]));
-            } else {
-                encoded.push(RleComponent::Run(counter, last));
-            }
-
-            counter = 1;
-            last = bv;
-        }
-    }
-    // TODO: This repeated code isn't ideal.
-    if counter == 1 {
-        encoded.push(RleComponent::Literal(vec![last]));
-    } else {
-        encoded.push(RleComponent::Run(counter, last));
-    }
-
-
-    encoded//.to_vec()
-}
-
-fn rld(input: Vec<RleComponent>) -> String {
-    let mut decoded = vec!();
-
-    for component in input {
-        match component {
-            RleComponent::Run(length, byte) => {
-                // TODO: Is there a better way to add
-                // repeated values to the end of a vec?
-                for _ in 0..length {
-                    decoded.push(byte);
-                }
-            },
-            RleComponent::Literal(bytes) => {
-                decoded.extend(bytes);
-            }
-        }
-    }
-
-    String::from_utf8(decoded).unwrap()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn basic_rle() {
-        assert_eq!(
-            rle("hello"),
-            [
-                RleComponent::Literal(vec![104]),
-                RleComponent::Literal(vec![101]),
-                RleComponent::Run(2, 108),
-                RleComponent::Literal(vec![111])
-            ]
-        );
-    }
-
-    #[test]
-    fn with_trailing_run() {
-        assert_eq!(
-            rle("helloo"),
-            [
-                RleComponent::Literal(vec![104]),
-                RleComponent::Literal(vec![101]),
-                RleComponent::Run(2, 108),
-                RleComponent::Run(2, 111),
-            ]
-        );
-    }
-
-    #[test]
-    fn moretests() {
-        assert_eq!(
-            rle("aaaaaab"),
-            [
-                RleComponent::Run(6, 97),
-                RleComponent::Literal(vec![98]),
-            ]
-        );
-    }
-
-
-    #[test]
-    fn basic_rld() {
-        assert_eq!(
-            rld(rle("aabb")),
-            "aabb"
-        );
-    }
-
-    #[test]
-    fn basic_rld_only_literal() {
-        assert_eq!(
-            rld(rle("ab")),
-            "ab"
-        );
-    }
 }
---
+++ src/run_length_encoding/mod.rs
@@ -0,0 +1,116 @@
+#[derive(Debug,PartialEq)]
+pub enum RleComponent {
+    Run(usize, u8),
+    Literal(Vec<u8>)
+}
+
+pub fn encode(input: &str) -> Vec<RleComponent> {
+    let mut last : u8 = input.bytes().nth(0).unwrap();
+    let mut counter : usize = 0;
+    let mut encoded = vec!();
+
+    for bv in input.bytes() {
+        if bv == last {
+            counter += 1;
+        } else {
+            if counter == 1 {
+                // TODO: Combine adjacent literals.
+                encoded.push(RleComponent::Literal(vec![last]));
+            } else {
+                encoded.push(RleComponent::Run(counter, last));
+            }
+
+            counter = 1;
+            last = bv;
+        }
+    }
+    // TODO: This repeated code isn't ideal.
+    if counter == 1 {
+        encoded.push(RleComponent::Literal(vec![last]));
+    } else {
+        encoded.push(RleComponent::Run(counter, last));
+    }
+
+
+    encoded//.to_vec()
+}
+
+pub fn decode(input: Vec<RleComponent>) -> String {
+    let mut decoded = vec!();
+
+    for component in input {
+        match component {
+            RleComponent::Run(length, byte) => {
+                // TODO: Is there a better way to add
+                // repeated values to the end of a vec?
+                for _ in 0..length {
+                    decoded.push(byte);
+                }
+            },
+            RleComponent::Literal(bytes) => {
+                decoded.extend(bytes);
+            }
+        }
+    }
+
+    String::from_utf8(decoded).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_encode() {
+        assert_eq!(
+            encode("hello"),
+            [
+                RleComponent::Literal(vec![104]),
+                RleComponent::Literal(vec![101]),
+                RleComponent::Run(2, 108),
+                RleComponent::Literal(vec![111])
+            ]
+        );
+    }
+
+    #[test]
+    fn with_trailing_run() {
+        assert_eq!(
+            encode("helloo"),
+            [
+                RleComponent::Literal(vec![104]),
+                RleComponent::Literal(vec![101]),
+                RleComponent::Run(2, 108),
+                RleComponent::Run(2, 111),
+            ]
+        );
+    }
+
+    #[test]
+    fn moretests() {
+        assert_eq!(
+            encode("aaaaaab"),
+            [
+                RleComponent::Run(6, 97),
+                RleComponent::Literal(vec![98]),
+            ]
+        );
+    }
+
+
+    #[test]
+    fn basic_decode() {
+        assert_eq!(
+            decode(encode("aabb")),
+            "aabb"
+        );
+    }
+
+    #[test]
+    fn basic_decode_only_literal() {
+        assert_eq!(
+            decode(encode("ab")),
+            "ab"
+        );
+    }
+}
